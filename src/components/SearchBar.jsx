e73668208a8c934f20c4f7620537e107f8ca7888
Kseniia <xeniiatikhomirova@gmail.com>
2024-06-05 20:11:11 +0400

    added file to gitignore

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 import styles from "./searchbars.module.css";
-import { API_KEY } from "./ApiKey";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
-
+const apiKey = process.env.REACT_APP_API_KEY;
 
 export default function SearchBar({ foodData, setFoodData }) {
   const [query, setQuery] = useState("sushi");
 
   useEffect(() => {
     async function fetchUrl() {
-      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const res = await fetch(`${URL}?query=${query}&apiKey=${apiKey}`);
       const data = await res.json();
       console.log(data.results);
       setFoodData(data.results);
